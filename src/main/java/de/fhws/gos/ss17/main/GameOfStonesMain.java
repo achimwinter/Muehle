142c830054cb71288ada4211d7d8b6d2bed883e0
Tobias Fertig <Tobias.Fertig@t-online.de>
2017-03-21 15:43:27 +0100

    initial commit

---
+++ src/main/java/de/fhws/gos/ss17/main/GameOfStonesMain.java
@@ -0,0 +1,42 @@
+package de.fhws.gos.ss17.main;
+
+import de.fhws.gos.core.logic.Board;
+import de.fhws.gos.core.logic.Game;
+import de.fhws.gos.core.network.Connection;
+import de.fhws.gos.game.impl.GameFactory;
+
+/**
+ * (c) Tobias Fertig, FHWS 2017
+ */
+public class GameOfStonesMain
+{
+	public static void main( String[] args )
+	{
+		GameOfStonesMain gameOfStonesMain = new GameOfStonesMain( );
+		gameOfStonesMain.generateGame( );
+		gameOfStonesMain.showWelcome( );
+		gameOfStonesMain.startGame( );
+	}
+
+	private Game game;
+
+	public void generateGame( )
+	{
+		Connection connection = Config.initConnection( );
+		Board board = Config.initBoard( );
+
+		this.game = GameFactory.getInstance( connection, board ).createGame( Config.GAME_MODE );
+		this.game.setPlayers( Config.getRandomPlayer( ), Config.getRemotePlayer( connection ) );
+	}
+
+	public void showWelcome( )
+	{
+		System.out.println( "Welcome to GameOfStones!" );
+	}
+
+	public void startGame( )
+	{
+		game.initGame( );
+		game.startGame( );
+	}
+}
---
+++ src/test/java/de/fhws/gos/ss17/main/ConfigTests.java
@@ -0,0 +1,81 @@
+package de.fhws.gos.ss17.main;
+
+import de.fhws.gos.core.network.Connection;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * (c) Tobias Fertig, FHWS 2017
+ */
+public class ConfigTests
+{
+	@Test
+	public void testInitConnection_IsImplemented_NoExceptionThrown( )
+	{
+		boolean exceptionThrown = false;
+
+		try
+		{
+			Config.initConnection( );
+		}
+		catch ( Exception e )
+		{
+			exceptionThrown = true;
+		}
+
+		assertFalse( "Exception was thrown, but shouldn't", exceptionThrown );
+	}
+
+	@Test
+	public void testInitBoard_IsImplemented_NoExceptionThrown( )
+	{
+		boolean exceptionThrown = false;
+
+		try
+		{
+			Config.initBoard( );
+		}
+		catch ( Exception e )
+		{
+			exceptionThrown = true;
+		}
+
+		assertFalse( "Exception was thrown, but shouldn't", exceptionThrown );
+	}
+
+	@Test
+	public void testGetRemotePlayer_IsImplemented_NoExceptionThrown( )
+	{
+		boolean exceptionThrown = false;
+
+		try
+		{
+			Connection connection = Config.initConnection( );
+			Config.getRemotePlayer( connection );
+		}
+		catch ( Exception e )
+		{
+			exceptionThrown = true;
+		}
+
+		assertFalse( "Exception was thrown, but shouldn't", exceptionThrown );
+	}
+
+	@Test
+	public void testGetRandomPlayer_IsImplemented_NoExceptionThrown( )
+	{
+		boolean exceptionThrown = false;
+
+		try
+		{
+			Config.getRandomPlayer( );
+		}
+		catch ( Exception e )
+		{
+			exceptionThrown = true;
+		}
+
+		assertFalse( "Exception was thrown, but shouldn't", exceptionThrown );
+	}
+}
