db4ddf962997a6ee101e6f0a444c95b83746d396
AWinter <achim.winter@student.fhws.de>
2017-04-04 21:42:22 +0200

    CyborgPlayer fixed

--- src/main/java/de/fhws/gos/ss17/players/CyborgPlayer.java
+++ src/main/java/de/fhws/gos/ss17/players/CyborgPlayer.java
@@ -4,62 +4,62 @@ import de.fhws.gos.core.exceptions.GameException;
 import de.fhws.gos.core.logic.Board;
 import de.fhws.gos.core.logic.Move;
 import de.fhws.gos.core.utils.PositionToken;
+import de.fhws.gos.players.impl.AbstractPlayer;
 import de.fhws.gos.players.impl.RandomPlayerWithRules;
 import java.util.Scanner;
 
 /**
  * Created by awinter on 04.04.17.
  */
-public class CyborgPlayer extends RandomPlayerWithRules{
+public class CyborgPlayer extends AbstractPlayer{
 
+  HumanPlayer human = new HumanPlayer(playerToken);
+  RandomPlayerWithRules randomPlayer = new RandomPlayerWithRules(playerToken);
 
-  private HumanPlayer human;
-  private RandomPlayerWithRules randomPlayer;
-  private Scanner input = new Scanner(System.in);
-
+  private Scanner sc = new Scanner(System.in);
   char change = 'n';
 
   public CyborgPlayer(PositionToken playerToken) {
     super(playerToken);
   }
 
-  protected Move getPlacingMove(Board board)throws GameException{
-    Move placement = super.getPlacingMove(board);
+
+  protected Move getPlacingMove(Board board) throws GameException {
+    Move placement = randomPlayer.getNextMove(board);
     System.out.println("Stein wird gesetzt auf: " + placement.getToId());
     System.out.print("Möchten Sie den Zug verändern?(y/n)");
-    change = input.findInLine(".").charAt(0);
+    change = sc.next(".").charAt(0);
 
-    if(change == 'y'){
+    if (change == 'y') {
       placement = human.getPlacingMove(board);
     }
     return placement;
   }
 
-  protected Move getMovingMove(Board board) throws GameException{
-    Move move = super.getMovingMove(board);
-    System.out.println("Stein wird verschoben von: " + move.getFromId() + " nach: " + move.getToId());
-    change = input.findInLine(".").charAt(0);
-
-    if(change == 'y'){
+  protected Move getMovingMove(Board board) throws GameException {
+    Move move = randomPlayer.getNextMove(board);
+    System.out
+        .println("Stein wird verschoben von: " + move.getFromId() + " nach: " + move.getToId());
+    System.out.print("Möchten Sie den Zug ändern? (y/n)");
+    change = sc.next(".").charAt(0);
+    if (change == 'y') {
       move = human.getMovingMove(board);
     }
     return move;
   }
 
 
-  protected Move getFlyingMove(Board board) throws GameException{
-    Move move = super.getFlyingMove(board);
-    System.out.println("Stein wird verschoben von: " + move.getFromId() + " nach: " + move.getToId());
-    change = input.findInLine(".").charAt(0);
+  protected Move getFlyingMove(Board board) throws GameException {
+    Move move = randomPlayer.getNextMove(board);
+    System.out
+        .println("Stein wird verschoben von: " + move.getFromId() + " nach: " + move.getToId());
+    change = sc.next(".").charAt(0);
 
-    if (change == 'y'){
+    if (change == 'y') {
       move = human.getFlyingMove(board);
     }
     return move;
   }
-
-
-
 
 
 }
