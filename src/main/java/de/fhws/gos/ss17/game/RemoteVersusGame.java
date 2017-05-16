77176c6a0d4c36e8e274990fe404f6b24a23b35b
Achim <achim.winter@student.fhws.de>
2017-05-16 21:06:42 +0200

    RemoteVersusGame erstellt. Funktioniert?

---
+++ src/main/java/de/fhws/gos/ss17/game/RemoteVersusGame.java
@@ -0,0 +1,49 @@
+package de.fhws.gos.ss17.game;
+
+import de.fhws.gos.core.logic.Player;
+import de.fhws.gos.core.network.Connection;
+import de.fhws.gos.core.utils.PositionToken;
+import de.fhws.gos.remote.utils.JSONHelper;
+import de.fhws.gos.remote.utils.models.ServerEntity;
+import java.io.IOException;
+
+/**
+ * Created by Achim on 16.05.2017.
+ */
+public class RemoteVersusGame extends AbstractGame {
+
+  private Connection connection;
+
+  public RemoteVersusGame(Board board, Connection connection) {
+    super(board);
+    this.connection = connection;
+  }
+
+  public RemoteVersusGame(Board board, Player playerOne,
+      Player playerTwo, Connection connection) {
+    super(board, playerOne, playerTwo);
+    this.connection = connection;
+  }
+
+  public void initGame(){
+    try {
+      String response = this.connection.joinGame();
+      ServerEntity entity = (new JSONHelper()).deserialize(response);
+      this.connection.setGameId(entity.getId());
+      if (entity.getTurnResult() != null) {
+        this.currentPlayer = this.playerTwo;
+        this.board.executeMove(entity.getTurnResult().getTurn().asMove(),
+            this.currentPlayer.getPlayerToken());
+        if(this.currentPlayer.equals(PositionToken.PLAYER_ONE)){
+          this.currentPlayer = playerTwo;
+        }else{
+          this.currentPlayer = playerOne;
+        }
+      } else {
+        this.currentPlayer = this.playerOne;
+      }
+    }catch (IOException ex){
+      ex.printStackTrace();
+    }
+  }
+}
