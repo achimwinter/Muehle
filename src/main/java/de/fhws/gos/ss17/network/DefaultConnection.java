21ab976d31a66dd221f355d4478a62d62ac9466a
awinter <achim.winter@yahoo.de>
2017-05-24 11:15:27 +0200

    updated

--- src/main/java/de/fhws/gos/ss17/network/DefaultConnection.java
+++ src/main/java/de/fhws/gos/ss17/network/DefaultConnection.java
@@ -15,7 +15,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
 
 /**
